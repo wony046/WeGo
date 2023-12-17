e97349b41f84cad46010ad6d18a140d0f15a7caf
ckdrhd <143183579+ckdrhd@users.noreply.github.com>
2023-12-17 19:57:15 +0900

    Update imu.py

--- scripts/lane_detection/imu.py
+++ scripts/lane_detection/imu.py
@@ -12,8 +12,8 @@ class limo_imu:
         self.linear_acceleration_x = 0
         self.linear_acceleration_z = 0
         self.pitch = 0
-        self.start_bump = math.pi / 30
-        self.last_bump = -(math.pi / 30)
+        self.start_bump = math.pi / 33
+        self.last_bump = -(math.pi / 33)
         self.loop_time = 0
         rospy.Subscriber("/imu", Imu, self.pitch_calculate_callback)
         self.imu_pub = rospy.Publisher("/limo/imu_pitch", String, queue_size=2)
@@ -25,7 +25,7 @@ class limo_imu:
             self.wait_time = rospy.get_time()
             self.pitch = math.atan(self.linear_acceleration_x / self.linear_acceleration_z)
             if (self.pitch >= self.start_bump or self.pitch <= self.last_bump):
-                if self.wait_time - self.loop_time >= 0.3:
+                if self.wait_time - self.loop_time >= 0.1:
                     self.imu_pub.publish("bump")
             else:
                 self.loop_time = rospy.get_time()
