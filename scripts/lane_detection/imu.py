233832474609d7533b2ad0ebb21dbff696abba27
wony046 <144529031+wony046@users.noreply.github.com>
2023-12-17 21:37:58 +0900

    Update imu.py

--- scripts/lane_detection/imu.py
+++ scripts/lane_detection/imu.py
@@ -12,23 +12,23 @@ class limo_imu:
         self.linear_acceleration_x = 0
         self.linear_acceleration_z = 0
         self.pitch = 0
-        self.start_bump = math.pi / 53
+        self.start_bump = math.pi / 40
         self.last_bump = -(math.pi / 40)
         self.loop_time = 0
         rospy.Subscriber("/imu", Imu, self.pitch_calculate_callback)
-        self.imu_pub = rospy.Publisher("/limo/imu_pitch", String, queue_size=2)
+        self.imu_pub = rospy.Publisher("/limo/imu_pitch", String, queue_size=1)
     
     def pitch_calculate_callback(self, data):
         self.linear_acceleration_x = data.linear_acceleration.x
         self.linear_acceleration_z = data.linear_acceleration.z
         if self.linear_acceleration_z != 0:
-            self.wait_time = rospy.get_time()
+            #self.wait_time = rospy.get_time()
             self.pitch = math.atan(self.linear_acceleration_x / self.linear_acceleration_z)
             if (self.pitch >= self.start_bump or self.pitch <= self.last_bump):
-                if self.wait_time - self.loop_time >= 0.03:
-                    self.imu_pub.publish("bump")
+                #if self.wait_time - self.loop_time >= 0.03:
+                self.imu_pub.publish("bump")
             else:
-                self.loop_time = rospy.get_time()
+                #self.loop_time = rospy.get_time()
                 self.imu_pub.publish("not_bump")
 
 def run():
@@ -40,4 +40,3 @@ if __name__ == '__main__':
         run()
     except rospy.ROSInterruptException:
         pass
-
