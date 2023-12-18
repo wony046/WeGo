0ffa33978dc06535f815bca496dfafda67c75834
wony046 <144529031+wony046@users.noreply.github.com>
2023-12-18 12:43:19 +0900

    Update imu.py

--- scripts/lane_detection/imu.py
+++ scripts/lane_detection/imu.py
@@ -4,25 +4,29 @@
 import rospy
 import math
 from sensor_msgs.msg import Imu
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 
 class limo_imu:
     def __init__(self):
         rospy.init_node('imu', anonymous=True)
         self.linear_acceleration_x = 0
         self.linear_acceleration_z = 0
+        self.linear_orientation_z = 0
         self.pitch = 0
         self.start_bump = math.pi / 36
         self.last_bump = -(math.pi / 36)
         self.loop_time = 0
         rospy.Subscriber("/imu", Imu, self.pitch_calculate_callback)
         self.imu_pub = rospy.Publisher("/limo/imu_pitch", String, queue_size=2)
+        self.imu_orientation_pub = rospy.Publisher("/limo/imu_roll", Float32, queue_size=2)
     
     def pitch_calculate_callback(self, data):
         #self.wait_time = rospy.get_time()
         self.linear_acceleration_x = data.linear_acceleration.x
         self.linear_acceleration_z = data.linear_acceleration.z
-        if ((self.linear_acceleration_x >= 2.2 or self.linear_acceleration_x <= -2.2) and self.linear_acceleration_z != 0):
+        self.linear_orientation_z = data.linear_orientation.z
+        self.imu_orientation_pub.publish(self.linear_orientation_z)
+        if ((self.linear_acceleration_x >= 1.9 or self.linear_acceleration_x <= -1.9) and self.linear_acceleration_z != 0):
             self.pitch = math.atan(self.linear_acceleration_x / self.linear_acceleration_z)
             if (self.pitch >= self.start_bump or self.pitch <= self.last_bump):
                 #if self.wait_time - self.loop_time >= 0.5:
